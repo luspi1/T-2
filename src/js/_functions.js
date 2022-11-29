cd8dc3e3508aefc0e03a0bdba05b143321980d18
VensKramskoy <venskramskoy@gmail.com>
2022-11-29 14:16:32 +0300

    закончена верстка стационарки страницы /добавить объект/

---
+++ src/js/_functions.js
@@ -0,0 +1,16 @@
+// Обрезка длинного текста на определенную длину
+
+const cutString = (stringArray) => {
+  stringArray.forEach(str => {
+    const cutLength = +str.dataset.shear
+
+    if (cutLength && cutLength < str.textContent.length) {
+      str.textContent = `${str.textContent.substring(0, cutLength)}...`
+    }
+  })
+}
+
+
+export {
+  cutString
+ }
---
+++ src/js/components/cutString.js
@@ -0,0 +1,6 @@
+import { cutString } from "../_functions";
+
+//обрезка длинных строк  на странице элемент шаблона
+
+const trimNewObjStrings = document.querySelectorAll('.new-object-form span[data-shear]');
+cutString(trimNewObjStrings);
