4404a1543f9f4fe259beae6c3ce1fc50e4019577
VensKramskoy <venskramskoy@gmail.com>
2022-11-30 17:17:27 +0300

    сделан dropzone фото объектов на странице /добавить объект /

--- src/js/components/dropzone.js
+++ src/js/components/dropzone.js
@@ -19,3 +19,20 @@ if (passportPhoto) {
 
   });
 }
+
+//Dropzone для фото объектов недвижимости в личном кабинете
+
+const newObjectPhoto = document.querySelector('#new-object-dropzone');
+
+if (newObjectPhoto) {
+  let newObjectDropzone = new Dropzone(newObjectPhoto, {
+    maxFilesize: 5,
+    url: "/include/ajax/upload_image.php",
+    maxFiles: 10,
+    thumbnailWidth: 93,
+    thumbnailHeight: 93,
+    acceptedFiles: '.png, .jpeg, .jpg',
+    addRemoveLinks: true,
+    clickable: '#photo-object-add',
+  });
+}
