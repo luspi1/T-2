a2fc0949709eaa483a9e3829d56ef0ee3a5a4ec1
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-03-14 12:00:39 +0300

    начало модалок регистрации/авторизации

--- src/js/components/langSwitch.js
+++ src/js/components/langSwitch.js
@@ -1,11 +1,14 @@
-const langSwitch = document.querySelector('.main-menu__lang-switch')
+const langSwitches = document.querySelectorAll('.main-menu__lang-switch')
 
-if (langSwitch) {
-  const switchBtns = langSwitch.querySelectorAll('.main-menu__lang-btn')
-  langSwitch.addEventListener('click', (e) => {
-    if (!e.target.classList.contains('main-menu__lang-btn_active')) {
-      switchBtns.forEach(btn => btn.classList.remove('main-menu__lang-btn_active'))
-      e.target.classList.add('main-menu__lang-btn_active')
-    }
+if (langSwitches) {
+  langSwitches.forEach(langSwitch => {
+    const switchBtns = langSwitch.querySelectorAll('.main-menu__lang-btn')
+    langSwitch.addEventListener('click', (e) => {
+      if (!e.target.classList.contains('main-menu__lang-btn_active')) {
+        switchBtns.forEach(btn => btn.classList.remove('main-menu__lang-btn_active'))
+        e.target.classList.add('main-menu__lang-btn_active')
+      }
+    })
   })
+
 }
