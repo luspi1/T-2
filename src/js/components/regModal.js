13c51104482fc6a7fe4e37486f54aeaae3ebed27
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-03-15 17:20:21 +0300

    часть валидации модалки регистрации

--- src/js/components/regModal.js
+++ src/js/components/regModal.js
@@ -3,16 +3,43 @@ import { Dropzone } from 'dropzone'
 const regModal = document.querySelector('#reg-modal')
 
 if (regModal) {
+
+  //Валидация
+
+  const changeValidInputs = (modalState) => {
+    const allValidateInputs = regModal.querySelectorAll('input, textarea')
+    allValidateInputs.forEach(el => el.required = false)
+    const targetValidateInputs = regModal.querySelectorAll(`.${modalState} input, .${modalState} textarea`)
+    targetValidateInputs.forEach(el => el.required = true)
+  }
+
   const controllBtns = regModal.querySelectorAll('button[data-btn-state]')
   if (controllBtns) {
     controllBtns.forEach(btn => {
       btn.addEventListener('click', (e) => {
+        if (e.currentTarget.classList.contains('reg-modal__next-btn')) {
+
+          const currentBlock = e.currentTarget.closest('.reg-modal__block')
+          const validateInputs = currentBlock.querySelectorAll('input, textarea')
+
+          const validateMessage = currentBlock.querySelector('.reg-modal__alert-change')
+
+          const isValid = Array.from(validateInputs).some(el => el.checkValidity())
+          if (!isValid) {
+            validateMessage.classList.remove('hidden')
+            return
+          } else {
+            validateMessage.classList.add('hidden')
+          }
+        }
+        changeValidInputs(e.currentTarget.dataset.btnState)
         regModal.dataset.state = e.currentTarget.dataset.btnState
       })
     })
   }
-}
 
+
+}
 
 
 //Dropzone для загрузки сканов
@@ -34,4 +61,12 @@ if (regScans) {
     clickable: '#reg-scans-add',
     previewsContainer: '.reg-scans-previews'
   })
+
+
 }
+
+
+
+
+
+
